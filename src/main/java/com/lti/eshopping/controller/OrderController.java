e118f8775b9996662212a984273603c5fe6f3dda
dasswayam <47634472+dasswayam@users.noreply.github.com>
2023-02-25 11:46:24 +0530

    Delete OrderController.java

--- src/main/java/com/lti/eshopping/controller/OrderController.java
+++
@@ -1,9 +0,0 @@
-package com.lti.eshopping.controller;
-
-public class OrderController {
-	
-	
-@RequestMapping(value = "/add-order", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-
-
-}
